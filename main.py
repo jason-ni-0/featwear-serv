75b32aca747049408648b77e65b4ef8c203285ad
Jason Ni <jasonni@ucsb.edu>
2022-09-17 10:19:07 -0700

    weather endpoint

--- main.py
+++ main.py
@@ -3,30 +3,58 @@ from bs4 import BeautifulSoup
 from flask import Flask,request
 from flask_restful import Resource, Api, reqparse
 import ast
+from datetime import date
+import config
 
 app = Flask(__name__)
 api = Api(app)
 
-class Wear(Resource):
+def getHoroscope(zodiac, day):
+    URL = "https://trustedteller.com/horoscope/"+zodiac+"/horoscope-"+day+"/luck"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup.find("p", class_="pre mb-0")
+
+def getHex(colors):
+    hexes = []
+    for color in colors:
+        color = color.replace(" ", "+")
+        URL = "https://alexbeals.com/projects/colorize/search.php?q=" + color
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        hexes.append(soup.find("span", class_="hex").string)
+    return hexes
+
+
+class Color(Resource):
     def get(self):
+        today = date.today()
         zodiac = request.args.get('zodiac')
         if zodiac == None:
             return {'message': 'Invalid Query'}
-        URL = "https://trustedteller.com/horoscope/"+zodiac+"/horoscope-today/luck"
-        page = requests.get(URL)
 
-        soup = BeautifulSoup(page.content, "html.parser")
+        horoscope = getHoroscope(zodiac, "today")
 
-        horoscope = soup.find("p", class_="pre mb-0")
-        #print(horoscope)
+        dateElem = horoscope.find("span", class_="date-horscop").string[:-2]
+        if dateElem[:2] != str(today)[-2:]:
+            horoscope = getHoroscope(zodiac, "yesterday")
+        dateElem = horoscope.find("span", class_="date-horscop").string[:-2]
+        hexes = getHex(horoscope.find_all("p")[0].string[20:].split())
+        return {'date': today.strftime("%B %d, %Y"), 'colors': horoscope.find_all("p")[0].string[20:].split(), 'hex':hexes}, 200  # return data and 200 OK code
 
-        #print(horoscope.find_all("p")[0].string)
+class Weather(Resource):
+    def get(self):
+        API_KEY = config.API_KEY
+        lat = request.args.get('lat')
+        long = request.args.get('long')
+        if (lat == None) or (long == None):
+            return {'message': 'Invalid Query'}
+        URL = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=imperial'
+        weather = requests.get(URL)
+        return {'name': weather.json()['name'], 'weather': weather.json()['weather'][0]['description'], 'temp': round(weather.json()['main']['temp']), 'min': round(weather.json()['main']['temp_min']), 'max': round(weather.json()['main']['temp_max'])}
 
-        dateElem = horoscope.find("span", class_="date-horscop").string
-        #print(dateElem.string)
-        return {'date': dateElem, 'colors': horoscope.find_all("p")[0].string}, 200  # return data and 200 OK code
-
-api.add_resource(Wear, '/wear')  # '/users' is our entry point
+api.add_resource(Color, '/color')
+api.add_resource(Weather, '/weather')
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
